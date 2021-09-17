5d558a16fee47d624976e4320f5530ebd4cd43bd
onspli <ondrej.splichal@gmail.com>
2021-09-17 22:28:19 +0200

    Add benchmark tests.

--- tests/benchmark/FENBench.php
+++ tests/benchmark/FENBench.php
@@ -21,6 +21,24 @@ class FENBench
   /**
   * @Revs(10)
   */
+  public function benchCopy()
+  {
+    $fen = new FEN('rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR b KQq c6 1 2');
+    $fen->copy();
+  }
+
+  /**
+  * @Revs(10)
+  */
+  public function benchMove()
+  {
+    $fen = new FEN("rn1qk1nr/pp1b1ppp/4p3/1B1p4/1b1P1B2/2N5/PPP2PPP/R2QK1NR w KQkq - 4 7");
+    $fen->move('Ne2');
+  }
+
+  /**
+  * @Revs(10)
+  */
   public function benchPossibleMovesInitialPosition()
   {
     $this->initial_pos->get_legal_moves();
